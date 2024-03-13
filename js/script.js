00e83e4bdca95825ac786fe498870d86acdecc7c
ZeeshanAbubakar <malikzeeshi325@gmail.com>
2024-03-14 01:34:50 +0500

    ASS

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.getElementById("first").innerHTML = "<span>this is span</span>"
+document.getElementById("btn").onclick = function () {
+    document.getElementById("heading").style.color = 'red'
+ }
+
+
+ document.getElementById("on").onclick = function (){
+    document.getElementById ("blub") .src = "https://www.w3schools.com/js/pic_bulbon.gif"
+ }
+
+
+ document.getElementById("off").onclick = function (){
+    document.getElementById ("blub") .src = "https://www.w3schools.com/js/pic_bulboff.gif"
+ }
+
+ 
+
+// alert("alert")
